4425a3302e6385696e32395d60d9dcd8155dd292
Adin Ashby <adin.ashby@gmail.com>
2022-10-31 07:47:08 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Programming Patterns - Project
+
+This template repository is the starter project for Programming Patterns Project. Written in Java, and tested with Gradle/JUnit.
+
+### Question(s)
+
+### Setup Command
+
+`gradle clean`
+
+### Run Command
+
+`gradle test`
---
+++ src/main/java/com/prog2/labs/Project.java
@@ -0,0 +1,22 @@
+package com.prog2.labs;
+
+/**
+ * @author adinashby
+ *
+ */
+
+public class Project {
+
+	/**
+	 * Write your test code below in the main (optional).
+	 *
+	 */
+	public static void main(String[] args) {
+
+	}
+
+	/**
+	 * Please refer to the README file for question(s) description
+	 */
+
+}
